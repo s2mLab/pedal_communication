0bb92f19eb428df848513c8ccae3cea9a4160ca3
Pariterre <pariterre@hotmail.com>
2025-12-05 17:31:57 -0500

    Ready to test UDP

--- pedal_communication/devices/__init__.py
+++ pedal_communication/devices/__init__.py
@@ -1,8 +1,11 @@
 from .tcp_pedal_device import TcpPedalDevice
-from .tpc_communication_protocol import TcpRequestProtocol, TcpResponseProtocol
+from .tpc_communication_protocol import TcpRequestProtocol
+from .udp_pedal_device import UdpPedalDevice
+from .udp_communication_protocol import UdpConfigurationProtocol
 
 __all__ = [
     TcpPedalDevice.__name__,
     TcpRequestProtocol.__name__,
-    TcpResponseProtocol.__name__,
+    UdpPedalDevice.__name__,
+    UdpConfigurationProtocol.__name__,
 ]
